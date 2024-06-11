b2b3d7c0bc2d46e127e8226a13e752c8a392fd8c
NurekeMuss <146543627+NurekeMuss@users.noreply.github.com>
2024-06-12 01:03:31 +0600

    ImageRecognize

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask import request
+import openai
+
+openai.api_key = 'sk-proj-EuO4xNezs8FkwA7GOoHfT3BlbkFJdhUIzJ6Lz9e4cJiIdkQZ'
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route("/generate-img", methods=['GET', 'POST'])
+def generate_img():
+    if request.method == 'POST':
+        try:
+            response = openai.Image.create(
+                prompt=f"draw {request.form['text']}",
+                n=1,
+                size="1024x1024"
+            )
+            image_url = response['data'][0]['url']
+            return f"""
+                <h1>{request.form['text']}</h1>
+                <img src='{image_url}' alt='Generated Image'>
+            """
+        except Exception as e:
+            return f'Ошибка генерации: {str(e)}'
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
